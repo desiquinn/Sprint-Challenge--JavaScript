9861cf0b4dbb84906c5502ff032cb485b17509b8
Desiree Morris <desiquinncompany@gmail.com>
2019-06-14 13:56:53 -0400

    completed stretch task to convert functions into arrow functions, Also complete stretch task to extend base class cuboid with a sub class called cubeMaker.

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,7 +23,32 @@ class CuboidMaker2 {
   });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid2.volume()); // 100
+console.log(cuboid2.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker2 {
+    constructor(cubeattr) {
+        super(cubeattr)
+    };
+
+    cubevolume() {
+        return `If it was a cube it's volume would be ${Math.pow(this.length, 3)}` 
+    };
+
+    cubeSurfaceArea() {
+        return `If it was a cube it's surface area would be ${6*(Math.pow(this.length, 2))}`
+    };
+};
+
+const cuboid3 = new CubeMaker ({
+    length: 4, 
+    width: 5, 
+    height: 5,
+  });
+
+console.log(cuboid3.cubevolume()); // 64
+console.log(cuboid3.cubeSurfaceArea()); //96
+console.log(cuboid3.volume()); // 100
+console.log(cuboid3.surfaceArea()); //130
