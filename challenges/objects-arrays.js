31d2a07f13ca64ff025c5e82b35ed3b5f44b8bb0
Desiree Morris <desiquinncompany@gmail.com>
2019-06-14 12:48:53 -0400

    created a new array called animalNames that includes the animal's name and scientific name

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -96,8 +96,11 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = universities.filter((universities) =>{
+  return universities >= 'Uni';
+})
 console.log(uni);
+console.log('number of universities with the string "Uni" in their name: ', uni.length);
 
 
 // ==== ADVANCED Array Methods ====
@@ -121,7 +124,12 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+
 const animalNames = [];
+
+zooAnimals.forEach((zooAnimals) => {
+  animalNames.push({name: zooAnimals.animal_name, scientific: zooAnimals.scientific_name})
+})
 console.log(animalNames);
 
 /* Request 2: .map()    
