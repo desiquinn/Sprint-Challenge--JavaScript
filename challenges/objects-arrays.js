9fd2b1455f1f95e649f6ae08830501a7bd0b419d
Desiree Morris <desiquinncompany@gmail.com>
2019-06-14 12:56:03 -0400

    created a new array containing the zoo animals names in lower case

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -138,7 +138,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map((zooAnimals) => {
+  return zooAnimals.animal_name.toLowerCase();
+});
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
